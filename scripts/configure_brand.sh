bbe82064ee3e95015022c9e6d214dda511bb6ee9
Jarosław Marek <jaroslaw.marek@gmail.com>
2022-03-02 23:44:22 +1300

    feat: Custom, brand specific, app icons

--- scripts/configure_brand.sh
+++ scripts/configure_brand.sh
@@ -13,11 +13,23 @@ for OPTION in "$@"; do
 done
 
 if [[ true == "$configureAndroid" ]]; then
-    echo -e "Configuring Android project..."
+    echo -e "\n=> Configuring Android project..."
     cp -rf android_template android
+
+    # Apply app icon
+    sourceDir="App/brands/$APP_BRAND/nativeAssets/appIcon/android"
+    echo -e "\n=> Copying brand app icon from: $sourceDir..."
+    [[ ! -d "$sourceDir" ]] && echo "Couldn't copy from $sourceDir. Directory doesn't exist" && exit 1
+    cp -rf $sourceDir/* "android/app/src/main/res/"
 fi
 
 if [[ true == "$configureIOS" ]]; then
-    echo -e "Configuring iOS project..."
+    echo -e "\n=> Configuring iOS project..."
     cp -rf ios_template ios
+
+    # Apply app icon
+    sourceDir="App/brands/$APP_BRAND/nativeAssets/appIcon/ios"
+    echo -e "\n=> Copying brand app icon from: $sourceDir..."
+    [[ ! -d "$sourceDir" ]] && echo "Couldn't copy from $sourceDir. Directory doesn't exist" && exit 1
+    cp -rf $sourceDir/* "ios/ReactNativeWLA/Images.xcassets/"
 fi
